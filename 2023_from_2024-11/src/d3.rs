a6a7da4ace6aeaa1e76e315385af1b7f0e51be0e
David Vogel <david@davidvogel.xyz>
2024-11-30 22:42:20 -0700

    completed day 3, part 2, from 2023
    got a bit more help on how to implement the hashmap. the key was also incorporating a hashset.

--- 2023_from_2024-11/src/d3.rs
+++ 2023_from_2024-11/src/d3.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::{HashMap, HashSet}, fs};
 
 pub fn read_file() -> String {
     let file_path = "inputs/d3_input.txt";
@@ -7,7 +7,7 @@ pub fn read_file() -> String {
         .expect("Should be able to read file.")
 }
 
-pub fn walk(input: &str) -> u32 {
+pub fn walk_part_one(input: &str) -> u32 {
     let grid = input
         .trim()
         .lines()
@@ -70,7 +70,78 @@ pub fn walk(input: &str) -> u32 {
     sum
 }
 
+pub fn walk_part_two(input: &str) -> u32 {
+    let grid = input
+        .trim()
+        .lines()
+        .map(|line| line
+            .trim()
+            .chars()
+            .collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut curr_num = 0;
+    let mut adj_star_positions: HashSet<(i32, i32)> = HashSet::new();
+    let mut stars: HashMap<(i32, i32), Vec<u32>> = HashMap::new();
+
+    for row in 0.. grid.len() {
+        for col in 0.. grid[row].len() {
+            let value = grid[row][col];
+
+            if !value.is_ascii_digit() {
+                continue
+            }
+
+            for row_offset in -1..=1 {
+                for col_offset in -1..=1 {
+                    if row_offset == 0 && col_offset == 0 {
+                        continue
+                    }
+
+                    let adj_row = row as i32 + row_offset;
+                    let adj_col = col as i32 + col_offset;
+
+                    if adj_row < 0
+                        || adj_row >= grid.len() as i32
+                            || adj_col < 0
+                            || adj_col >=grid[adj_row as usize].len() as i32
+                    {
+                        continue
+                    }
+
+                    if grid[adj_row as usize][adj_col as usize] == '*' {
+                        adj_star_positions.insert((adj_row, adj_col));
+                    }
+                }
+            }
+
+            curr_num *= 10;
+            curr_num += value.to_digit(10).unwrap();
+
+            if col + 1 >= grid[row].len() || !grid[row][col + 1].is_ascii_digit() {
+                for star in &adj_star_positions {
+                    stars.entry(*star).or_default().push(curr_num);
+                }
+
+                curr_num = 0;
+                adj_star_positions.clear();
+            }
+        }
+    }
+
+    stars
+        .values()
+        .filter(|numbers| numbers.len() == 2)
+        .map(|numbers| numbers[0] * numbers[1])
+        .sum()
+}
+
 pub fn day_three_part_one() -> u32 {
     let contents = read_file();
-    walk(&contents)
+    walk_part_one(&contents)
+}
+
+pub fn day_three_part_two() -> u32 {
+    let contents = read_file();
+    walk_part_two(&contents)
 }
