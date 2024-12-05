5de9ecce09694738d88f8ffb5542e6ec86d733ad
David Vogel <david@davidvogel.xyz>
2024-12-04 23:26:21 -0500

    almost completed day 2, part 2, from 2024
    edge case: what happens when first 2 numbers are the same, and the program can't figure out "increasing" or "decreasing"?

--- 2024_from_2024-12/rust/src/d2.rs
+++ 2024_from_2024-12/rust/src/d2.rs
@@ -5,7 +5,7 @@ pub fn read_file(file: &str) -> String {
         .expect("Should be able to read file.")
 }
 
-pub fn parse(input: String) -> i32 {
+pub fn parse_part_one(input: String) -> i32 {
     let mut total = 0;
 
     for line in input.lines() {
@@ -69,10 +69,149 @@ pub fn parse(input: String) -> i32 {
      total
 }
 
+pub fn parse_part_two(input: String) -> i32 {
+    let mut total = 0;
+
+    for line in input.lines() {
+        println!("attempting line: {}", line);
+        //let arr: Vec<&str> = line.trim().split_whitespace().collect::<Vec<_>>();
+        let line = line.trim().split_whitespace().collect::<Vec<_>>();
+        let line_iter = line.iter();
+        let mut v: Vec<i32> = Vec::new();
+
+        for item in line_iter {
+            let value = item.parse::<i32>().unwrap();
+            v.push(value);
+        }
+
+        let mut v_iter = v.iter();
+        let mut prev = v_iter.next().expect("num");
+        let mut curr_num = v_iter.next().expect("num");
+
+        let is_increasing;
+        let mut is_safe = true;
+        let mut diff;
+
+        if curr_num < prev {
+            is_increasing = false;
+        } else if curr_num > prev {
+            is_increasing = true;
+        } else {
+            continue;
+        }
+
+        if is_increasing == false {
+            diff = prev - curr_num;
+        } else {
+            diff = curr_num - prev;
+        }
+
+        if diff < 1 || diff > 3 {
+            continue;
+        }
+
+        for item in v_iter.clone() {
+            prev = curr_num;
+            curr_num = item;
+
+            if is_increasing == false {
+                diff = prev - curr_num;
+            } else {
+                diff = curr_num - prev;
+            }
+
+            if diff < 1 || diff > 3 {
+                is_safe = false;
+                break;
+            }
+        }
+
+        // DEBUG
+        if is_safe == true {
+            println!("THIS ONE SUCCEEDED WITHOUT MANIPULATION: {:?}", v);
+        }
+
+        if is_safe == false {
+            let mut n = 0;
+
+            while n < v.len() {
+                let mut v_clone = v.clone();
+                v_clone.remove(n);
+                let mut v_clone_iter = v_clone.iter();
+                let mut prev = v_clone_iter.next().expect("num");
+                let mut curr_num = v_clone_iter.next().expect("num");
+                n += 1;
+
+                let is_increasing;
+                is_safe = true;
+                let mut diff;
+
+                if curr_num < prev {
+                    is_increasing = false;
+                } else if curr_num > prev {
+                    is_increasing = true;
+                } else {
+                    continue;
+                }
+
+                if is_increasing == false {
+                    diff = prev - curr_num;
+                } else {
+                    diff = curr_num - prev;
+                }
+
+                if diff < 1 || diff > 3 {
+                    continue;
+                }
+
+                for item in v_clone_iter.clone() {
+                    prev = curr_num;
+                    curr_num = item;
+
+                    if is_increasing == false {
+                        diff = prev - curr_num;
+                    } else {
+                        diff = curr_num - prev;
+                    }
+
+                    if diff < 1 || diff > 3 {
+                        is_safe = false;
+                        break;
+                    }
+
+                }
+
+                if is_safe == true {
+                    // DEBUG
+                    println!("THIS ONE WAS PROBLEM DAMPENED: {:?}", v_clone);
+                    break;
+                }
+            }
+
+            // DEBUG
+            if is_safe == false {
+                println!("THIS ONE COMPLETELY FAILED: {:?}", v);
+            }
+        }
+
+        if is_safe == true {
+            total += 1
+        }
+    }
+
+    total
+}
+
 pub fn day_two_part_one(file: &str) -> i32 {
     let contents = read_file(file);
 
-    parse(contents)
+    parse_part_one(contents)
+}
+
+pub fn day_two_part_two(file: &str) -> i32 {
+    let contents = read_file(file);
+
+    parse_part_two(contents)
 }
 
 #[cfg(test)]
@@ -84,5 +223,12 @@ mod tests {
         let file = "../inputs/d2_test.txt";
 
         assert_eq!(2, day_two_part_one(file))
+    }
+
+    #[test]
+    fn day_two_part_two_test() {
+        let file = "../inputs/d2_test.txt";
+
+        assert_eq!(4, day_two_part_two(file))
     }
 }
--- 2024_from_2024-12/rust/src/main.rs
+++ 2024_from_2024-12/rust/src/main.rs
@@ -1,8 +1,8 @@
 mod d2;
-use d2::day_two_part_one;
+use d2::day_two_part_two;
 
 fn main() {
     let file = "../inputs/d2.txt";
 
-    dbg!(day_two_part_one(file));
+    dbg!(day_two_part_two(file));
 }
