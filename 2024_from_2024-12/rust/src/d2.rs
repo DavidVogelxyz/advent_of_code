dd3b7d5114b922354077c26f7194f113b7f439fd
David Vogel <david@davidvogel.xyz>
2024-12-05 10:05:11 -0500

    abstracted out function in day 2, part 1, from 2024
    this will help with day 2, part 2, as the function can be used again

--- 2024_from_2024-12/rust/src/d2.rs
+++ 2024_from_2024-12/rust/src/d2.rs
@@ -9,56 +9,58 @@ pub fn parse_part_one(input: String) -> i32 {
     let mut total = 0;
 
     for line in input.lines() {
-        let line = line.trim().split_whitespace().collect::<Vec<_>>();
-        let line_iter = line.iter();
-        let mut v: Vec<i32> = Vec::new();
+        let exit_code = proc_anal(line);
 
-        for item in line_iter {
-            let value = item.parse::<i32>().unwrap();
-            v.push(value);
-        }
-
-        let mut prev = v[0];
-        let mut curr_num = v[1];
-
-        let is_increasing;
-        let mut is_safe = true;
-
-        if curr_num < prev {
-            is_increasing = false;
-        } else if curr_num > prev {
-            is_increasing = true;
-        } else {
-            continue
-        }
-
-        for i in 1.. v.len() {
-            prev = v[i - 1];
-            curr_num = v[i];
-
-            let diff = prev - curr_num;
-            let diff_abs = diff.abs();
-
-            if diff_abs < 1 || diff_abs > 3 {
-                is_safe = false;
-                continue
-            }
-
-            if is_increasing == true && prev > curr_num {
-                is_safe = false;
-                continue
-            } if is_increasing == false && curr_num > prev {
-                is_safe = false;
-                continue
-            }
-        }
-
-        if is_safe == true {
+        if exit_code == 0 {
             total += 1;
         }
     }
 
     total
+}
+
+pub fn proc_anal(line: &str) -> i32 {
+    let line = line.trim().split_whitespace().collect::<Vec<_>>();
+    let line_iter = line.iter();
+    let mut v: Vec<i32> = Vec::new();
+
+    for item in line_iter {
+        let value = item.parse::<i32>().unwrap();
+        v.push(value);
+    }
+
+    let mut prev = v[0];
+    let mut curr_num = v[1];
+
+    let is_increasing;
+
+    if curr_num < prev {
+        is_increasing = false;
+    } else if curr_num > prev {
+        is_increasing = true;
+    } else {
+        return 1
+    }
+
+    for i in 1.. v.len() {
+        prev = v[i - 1];
+        curr_num = v[i];
+
+        let diff = prev - curr_num;
+        let diff_abs = diff.abs();
+
+        if diff_abs < 1 || diff_abs > 3 {
+            return 1
+        }
+
+        if is_increasing == true && prev > curr_num {
+            return 1
+        } if is_increasing == false && curr_num > prev {
+            return 1
+        }
+    }
+
+    return 0
 }
 
 pub fn parse_part_two(input: String) -> i32 {
