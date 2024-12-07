d7b1342452b54b546c9735b59be7b2aeac16de73
David Vogel <david@davidvogel.xyz>
2024-12-07 15:20:13 -0500

    incorrect solution to day 4, part 2, from 2024
    keeping this commit (and not squashing it) only because it contains code that successfully passed the test for day 4, part 2. however, for obvious reasons, it was not the correct solution.
    
    the "obvious reason" related to `adj_check_next` receiving the wrong arguments in two ways:
    1. when looking for "S", the function received the coordinates for "M", when it should have received the coordinates for "A"
    2. when looking for "S", the function should look at the "inverse offset" of the offset to "M", relative to "A"

--- 2024_from_2024-12/rust/src/main.rs
+++ 2024_from_2024-12/rust/src/main.rs
@@ -1,8 +1,8 @@
 mod d4;
-use d4::day_four_part_one;
+use d4::day_four_part_two;
 
 fn main() {
     let file = "../inputs/d4.txt";
 
-    dbg!(day_four_part_one(file));
+    dbg!(day_four_part_two(file));
 }
