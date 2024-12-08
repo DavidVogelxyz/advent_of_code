41836ab42984d5608a41d8700d4231bef5d69aea
David Vogel <david@davidvogel.xyz>
2024-12-07 21:35:19 -0500

    completed day 5, part 1, from 2024

--- 2024_from_2024-12/rust/src/main.rs
+++ 2024_from_2024-12/rust/src/main.rs
@@ -1,8 +1,8 @@
-mod d4;
-use d4::day_four_part_two;
+mod d5;
+use d5::day_five_part_one;
 
 fn main() {
-    let file = "../inputs/d4.txt";
+    let file = "../inputs/d5.txt";
 
-    dbg!(day_four_part_two(file));
+    dbg!(day_five_part_one(file));
 }
