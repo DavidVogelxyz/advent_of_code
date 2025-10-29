446da118adf179f11fb7ea0594aa74bf29988035
David Vogel <david@davidvogel.xyz>
2025-10-28 20:50:57 -0600

    UPDATE: completed "2020, day 1, part 2" in Bash

--- 2025-10_AoC-2020/bash/day01.sh
+++ 2025-10_AoC-2020/bash/day01.sh
@@ -2,7 +2,7 @@
 
 INPUT_FILE="../inputs/d01.txt"
 
-add_array() {
+add_array_part_one() {
     local total="${#arr[@]}"
 
     for ((i = 0; i < ((total - 1)); i++)); do
@@ -22,10 +22,33 @@ add_array() {
     fi
 }
 
+add_array_part_two() {
+    local total="${#arr[@]}"
+
+    for ((i = 0; i < ((total - 2)); i++)); do
+        for ((j = i + 1; j < ((total - 1)); j++)); do
+            for ((k = i + 2; k < total; k++)); do
+                if (( arr[i] + arr[j] + arr[k] == 2020 )); then
+                    l="${arr[i]}"
+                    m="${arr[j]}"
+                    r="${arr[k]}"
+                    break 3
+                fi
+            done
+        done
+    done
+
+    if ! [[ "$l" ]]; then
+        echo "Something went wrong... 😭😭😭" && exit 1
+    else
+        echo "The product of ${l}, ${m}, and ${r} = $((l * m * r))"
+    fi
+}
+
 main() {
     local arr=($(<"${INPUT_FILE}"))
 
-    add_array
+    add_array_part_two
 }
 
 main
