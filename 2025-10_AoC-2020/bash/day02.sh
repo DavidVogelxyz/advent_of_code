d67d3b3b261988d327968c70fdb67bf3de4db060
David Vogel <david@davidvogel.xyz>
2025-10-28 21:57:33 -0600

    UPDATE: completed "2020, day 2, part 2" in Bash

--- 2025-10_AoC-2020/bash/day02.sh
+++ 2025-10_AoC-2020/bash/day02.sh
@@ -2,7 +2,7 @@
 
 INPUT_FILE="../inputs/d02.txt"
 
-main() {
+valid_part_one() {
     local sum=0
 
     while read -r range char string; do
@@ -27,6 +27,37 @@ main() {
     done < "$INPUT_FILE"
 
     echo "The # of valid passwords = $sum"
+}
+
+valid_part_two() {
+    local sum=0
+
+    while read -r range char string; do
+        # Get the lower bound
+        local l="${range%%-*}"
+        l=$((l - 1))
+
+        # Get the upper bound
+        local u="${range##*-}"
+        u=$((u - 1))
+
+        # Get the char of interest
+        local c="${char%%:*}"
+
+        if [[ "${string:$l:1}" == "$c" ]]; then
+            if ! [[ "${string:$u:1}" == "$c" ]]; then
+                sum=$((sum + 1))
+            fi
+        elif [[ "${string:$u:1}" == "$c" ]]; then
+            sum=$((sum + 1))
+        fi
+    done < "$INPUT_FILE"
+
+    echo "The # of valid passwords = $sum"
+}
+
+main() {
+    valid_part_two
 }
 
 main
