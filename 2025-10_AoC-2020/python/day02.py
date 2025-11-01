0279ddb7c581da026c43d32ddb2fbce3f3c075ae
David Vogel <david@davidvogel.xyz>
2025-10-31 23:11:27 -0600

    UPDATE: completed "2020, day 2, part 2" in Python

--- 2025-10_AoC-2020/python/day02.py
+++ 2025-10_AoC-2020/python/day02.py
@@ -30,11 +30,47 @@ def day_two_part_one(arr):
     return sum
 
 
+def day_two_part_two(arr):
+    sum = 0
+
+    for line in arr:
+        # Get chunks
+        chunks = line.split()
+        numbers = chunks[0]
+        policy = chunks[1]
+        string = chunks[2]
+
+        # Parse chunks
+        bounds = numbers.split("-")
+        slice = policy.split(":")
+
+        # Get the low bound
+        low = int(bounds[0])
+        lo = string[low - 1:low]
+
+        # Get the high bound
+        high = int(bounds[1])
+        hi = string[high - 1:high]
+
+        # Get the char of interest
+        c = slice[0]
+
+        if c == lo:
+            if c != hi:
+                sum += 1
+        elif c == hi:
+            if c != lo:
+                sum += 1
+
+    return sum
+
+
 def main():
     with open("../inputs/d02.txt", "r") as file:
         arr = [line.strip() for line in file]
 
-    sum = day_two_part_one(arr)
+    #sum = day_two_part_one(arr)
+    sum = day_two_part_two(arr)
 
     print(f"The sum is {sum}")
 
