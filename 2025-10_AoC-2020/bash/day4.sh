4b6abb3b9b6a413773567e4b0473786df466c3a6
David Vogel <david@davidvogel.xyz>
2025-11-13 22:25:18 -0700

    UPDATE: completed "2020, day 4, part 2" in Bash

--- 2025-10_AoC-2020/bash/day4.sh
+++ 2025-10_AoC-2020/bash/day4.sh
@@ -12,7 +12,17 @@ must_contain=(
     "pid"
 )
 
-parse_line() {
+eye_colors=(
+    "amb"
+    "blu"
+    "brn"
+    "gry"
+    "grn"
+    "hzl"
+    "oth"
+)
+
+parse_line_part_one() {
     arr=($1)
 
     for item in "${arr[@]}"; do
@@ -44,7 +54,132 @@ day_four_part_one() {
 
     while read -r line; do
         if ! [[ "$line" == "" ]]; then
-            parse_line "$line"
+            parse_line_part_one "$line"
+        else
+            check_passport "${passport[*]}"
+            is_passport_valid
+
+            passport=()
+            pass_num=$((pass_num + 1))
+            valid=true
+        fi
+    done < "$INPUT_FILE"
+
+    # Check the last passport
+    check_passport "${passport[*]}"
+    is_passport_valid
+
+    echo "There are $sum valid passports!"
+}
+
+parse_line_part_two() {
+    arr=($1)
+
+    for item in "${arr[@]}"; do
+        # If the previous item was invalid, break out of `for` loop
+        # This also avoids continuing after moving to a new line
+        if [[ "$valid" == false ]]; then
+            break
+        fi
+
+        field="${item%%:*}"
+        value="${item##*:}"
+
+        case "$field" in
+            "byr")
+                if (( value >= 1920 && value <= 2002 )); then
+                    passport+=("$field")
+                else
+                    valid=false
+                fi;;
+            "iyr")
+                if (( value >= 2010 && value <= 2020 )); then
+                    passport+=("$field")
+                else
+                    valid=false
+                fi;;
+            "eyr")
+                if (( value >= 2020 && value <= 2030 )); then
+                    passport+=("$field")
+                else
+                    valid=false
+                fi;;
+            "hgt")
+                if [[ "$value" == *"cm" ]]; then
+                    # remove the units
+                    cm="${value%%cm}"
+
+                    # check the centimeter range
+                    if (( cm >= 150 && cm <= 193 )); then
+                        passport+=("$field")
+                    else
+                        valid=false
+                    fi
+                elif [[ "$value" == *"in" ]]; then
+                    # remove the units
+                    in="${value%%in}"
+
+                    # check the inches range
+                    if (( in >= 59 && in <= 76 )); then
+                        passport+=("$field")
+                    else
+                        valid=false
+                    fi
+                else
+                    valid=false
+                fi;;
+            "hcl")
+                # Remove strings that aren't hex values starting with `#`
+                if [[ "$value" =~ ^#[0-9a-f]+$ ]]; then
+                    # Prefix `0x` to the hex string, and remove the `#`
+                    value="0x${value###}"
+                else
+                    valid=false
+                    break
+                fi
+
+                # Process the hex string (this also confirms 6 digits)
+                if (( value >= 0x000000 && value <= 0xffffff )); then
+                    passport+=("$field")
+                else
+                    valid=false
+                fi;;
+            "ecl")
+                if [[ "${eye_colors[*]}" =~ "$value" ]]; then
+                    passport+=("$field")
+                else
+                    valid=false
+                fi;;
+            "pid")
+                # If `pid` has characters that aren't numbers, break
+                if ! [[ "$value" =~ ^[0-9]+$ ]]; then
+                    valid=false
+                    break
+                fi
+
+                digits_in_pid="${#value}"
+
+                # `pid` must contain 9 digits
+                if (( digits_in_pid == 9 )); then
+                    passport+=("$field")
+                else
+                    valid=false
+                fi;;
+            "cid")
+                continue;;
+        esac
+    done
+}
+
+day_four_part_two() {
+    passport=()
+    pass_num=1
+    sum=0
+    valid=true
+
+    while read -r line; do
+        if ! [[ "$line" == "" ]]; then
+            parse_line_part_two "$line"
         else
             check_passport "${passport[*]}"
             is_passport_valid
@@ -63,7 +198,11 @@ day_four_part_one() {
 }
 
 main() {
-    day_four_part_one
+    # PART ONE
+    #day_four_part_one
+
+    # PART TWO
+    day_four_part_two
 }
 
 main
