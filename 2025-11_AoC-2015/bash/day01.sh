b1773378f6fe03765fb87714371a014fa5b1a62d
David Vogel <david@davidvogel.xyz>
2025-11-21 23:18:23 -0700

    UPDATE: completed "2015, day 1, part 2" in Bash

--- 2025-11_AoC-2015/bash/day01.sh
+++ 2025-11_AoC-2015/bash/day01.sh
@@ -22,9 +22,36 @@ part_one() {
     echo "Santa will arrive on floor #${sum}."
 }
 
+part_two() {
+    local sum=0
+
+    while read -r line; do
+        local count="${#line}"
+
+        for ((i=0; i < count; i++)); do
+            local c="${line:i:1}"
+
+            if [[ "$c" == "(" ]]; then
+                sum="$((sum + 1))"
+            elif [[ "$c" == ")" ]]; then
+                sum="$((sum - 1))"
+            fi
+
+            if ((sum == -1)); then
+                # According to AoC, if the first character was a `)`, that would be position `1`, not `0`.
+                echo "The position of the first character that causes Santa to arrive at floor \`-1\` is position $((i + 1))."
+                break
+            fi
+        done
+    done < "$INPUT_FILE"
+}
+
 main() {
     # PART ONE
-    part_one
+    #part_one
+
+    # PART TWO
+    part_two
 }
 
 main
