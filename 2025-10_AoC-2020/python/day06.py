5ed222adf14e3a6dfced5fe68912dff9263bbf68
David Vogel <david@davidvogel.xyz>
2025-11-22 00:29:26 -0700

    UPDATE: vastly simplified logic for "2020, day 6" in Python

--- 2025-10_AoC-2020/python/day06.py
+++ 2025-10_AoC-2020/python/day06.py
@@ -1,90 +1,32 @@
-def parse_line_part_one(line, group):
-    count = int(len(line))
-    i = 0
-
-    while i < count:
-        letter = line[i:i + 1]
-
-        # if the `group` array does not contain the letter, add it
-        if group.__contains__(letter) == False:
-            group += [letter]
-
-        i += 1
-
-    return group
-
-
 def day_six_part_one(arr):
-    group = []
     sum = 0
 
-    for line in arr:
-        if line != "":
-            group = parse_line_part_one(line, group)
-        else:
-            count = int(len(group))
-            sum = sum + count
-            group = []
+    for group in arr:
+        for letter in "abcdefghijklmnopqrstuvwxyz":
+            if letter in group:
+                sum = sum + 1
 
-    count = int(len(group))
-    sum = sum + count
-
-    print(f"The sum is {sum}.")
-
-
-def parse_line_part_two(line, group, person):
-    count = int(len(line))
-    person = person + 1
-
-    if person == 1:
-        i = 0
-
-        while i < count:
-            letter = line[i:i + 1]
-
-            # if the `group` array does not contain the letter, add it
-            if group.__contains__(letter) == False:
-                group += [letter]
-
-            i += 1
-    else:
-        count_group = int(len(group))
-        j = count_group - 1
-
-        while j >= 0:
-            letter = group[j]
-
-            if letter not in line:
-                group.pop(j)
-
-            j += -1
-
-    return group, person
+    print(f"PART ONE: the sum is {sum}.")
 
 
 def day_six_part_two(arr):
-    group = []
-    person = 0
     sum = 0
 
-    for line in arr:
-        if line != "":
-            group, person = parse_line_part_two(line, group, person)
-        else:
-            count = int(len(group))
-            sum = sum + count
-            group = []
-            person = 0
+    for group in arr:
+        group = group.splitlines()
+        check = set(group[0])
 
-    count = int(len(group))
-    sum = sum + count
+        for person in group:
+            check = check & set(person)
 
-    print(f"The sum is {sum}.")
+        sum = sum + int(len(check))
+
+    print(f"PART TWO: the sum is {sum}.")
 
 
 def main():
     with open("../inputs/d06.txt", "r") as file:
-        arr = [line.strip() for line in file]
+        arr = file.read().strip("\n").split("\n\n")
 
     # PART ONE
     #day_six_part_one(arr)
