e64f625654a9cecb049a736231bdb3e257d9736b
David Vogel <david@davidvogel.xyz>
2025-11-25 23:11:47 -0700

    UPDATE: completed "2020, day 7, part 2" in Bash

--- 2025-10_AoC-2020/bash/day07.sh
+++ 2025-10_AoC-2020/bash/day07.sh
@@ -32,9 +32,38 @@ part_one() {
     echo "${#bags[@]} bags could (potentially) contain a shiny gold bag."
 }
 
+recursive_search() {
+    local term="${1#* }"
+    local count_bag=${1// *}
+    local count_inside=0
+    local count_recurse=0
+
+    while read -r line; do
+        if [[ "$line" =~ ^$term ]]; then
+            line=(${line##*contain })
+
+            for ((i=0; i < "${#line[@]}"; i+=4)); do
+                count_recurse="$(recursive_search "${line[$i]} ${line[((i + 1))]} ${line[((i + 2))]}")"
+                count_inside="$((count_inside + count_recurse))"
+            done
+        fi
+    done < "$INPUT_FILE"
+
+    echo $((count_bag + (count_bag * count_inside)))
+}
+
+part_two() {
+    local bags_total="$(recursive_search "1 shiny gold")"
+
+    echo "The shiny gold bag MUST CONTAIN $((bags_total - 1)) bags."
+}
+
 main() {
     # PART ONE
-    part_one
+    #part_one
+
+    # PART TWO
+    part_two
 }
 
 main
