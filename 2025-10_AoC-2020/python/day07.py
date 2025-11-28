76e3a8533d53fa6959df2bc0ad6798a2344858d7
David Vogel <david@davidvogel.xyz>
2025-11-28 00:02:23 -0700

    UPDATE: completed "2020, day 7, part 2" in Python

--- 2025-10_AoC-2020/python/day07.py
+++ 2025-10_AoC-2020/python/day07.py
@@ -25,12 +25,45 @@ def part_one(arr):
     print(f"{len(bags)} bags could (potentially) contain a shiny gold bag.")
 
 
+def recursive_search(arr, search_term):
+    count_inside = 0
+    count_recurse = 0
+
+    search_words = search_term.split(" ")
+    count_bag = search_words[0]
+    bag = f"{search_words[1]} {search_words[2]}"
+    for line in arr:
+        if line.startswith(bag):
+            bags_inside = line.split("contain ")
+            bag_words = bags_inside[1].split(", ")
+
+            for word in bag_words:
+                bunks = word.split(" ")
+                if bunks[0] == "no":
+                    return int(count_bag)
+                search_term = f"{bunks[0]} {bunks[1]} {bunks[2]}"
+                count_recurse = int(recursive_search(arr, search_term))
+                count_inside = int(count_inside) + int(count_recurse)
+
+    total = int(count_bag) + (int(count_bag) * int(count_inside))
+    return total
+
+
+def part_two(arr):
+    bags_total = recursive_search(arr, "1 shiny gold")
+
+    print(f"The shiny gold bag MUST CONTAIN {bags_total - 1} bags.")
+
+
 def main():
     with open("../inputs/d07.txt", "r") as file:
         arr = file.read().strip("\n").split("\n")
 
     # PART ONE
     part_one(arr)
+
+    # PART TWO
+    part_two(arr)
 
 
 if __name__ == "__main__":
