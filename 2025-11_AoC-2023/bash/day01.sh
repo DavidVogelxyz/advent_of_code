3f1d8de346475071536c26411ea416858af2a79b
David Vogel <david@davidvogel.xyz>
2025-12-01 20:48:20 -0700

    UPDATE: completed "2023, day 1, part 2" in Bash

--- 2025-11_AoC-2023/bash/day01.sh
+++ 2025-11_AoC-2023/bash/day01.sh
@@ -18,9 +18,39 @@ part_one() {
     echo "2023 D01 P1 = $sum"
 }
 
+part_two() {
+    local sum=0
+
+    while read -r line; do
+        # Swap words for numbers, but keep bordering letters in case of overlap
+        line="${line//one/o1e}"
+        line="${line//two/t2e}"
+        line="${line//three/t3e}"
+        line="${line//four/f4r}"
+        line="${line//five/f5e}"
+        line="${line//six/s6x}"
+        line="${line//seven/s7n}"
+        line="${line//eight/e8t}"
+        line="${line//nine/n9e}"
+
+        # Keep only nums from `$line`
+        local num="${line//[^0-9]/}"
+
+        # From `$num`, take first and last num and create new num
+        num="${num:0:1}${num: -1:1}"
+
+        sum="$((sum + num))"
+    done < "$INPUT_FILE"
+
+    echo "2023 D01 P2 = $sum"
+}
+
 main() {
     # PART ONE
     part_one
+
+    # PART TWO
+    part_two
 }
 
 main
