66579dc9673648bbe8498d749b71e9a0dd32b271
David Vogel <david@davidvogel.xyz>
2025-12-02 21:09:38 -0700

    UPDATE: completed "2025, day 2, part 1" in Python

---
+++ 2025-12_AoC-2025/python/day02.py
@@ -0,0 +1,52 @@
+def is_repetitive_part_one(num):
+    length = int(len(num))
+
+    if length % 2 == 1:
+        return False
+
+    mid = int(length / 2)
+    left = num[0:mid]
+    right = num[mid:]
+
+    if right.startswith("0"):
+        return False
+
+    if left == right:
+        return True
+    else:
+        return False
+
+
+def part_one(arr):
+    sum = 0
+
+    for line in arr:
+        chunks = line.split(",")
+
+        for chunk in chunks:
+            if chunk == "":
+                continue
+
+            dunks = chunk.split("-")
+            lo = int(dunks[0])
+            hi = int(dunks[1])
+
+            while lo < hi:
+                if is_repetitive_part_one(str(lo)):
+                    sum += lo
+
+                lo += 1
+
+    print(f"2025 D02 P1 = {sum}")
+
+
+def main():
+    with open("../inputs/d02.txt", "r") as file:
+        arr = file.read().strip("\n").split("\n")
+
+    # PART ONE
+    part_one(arr)
+
+
+if __name__ == "__main__":
+    main()
