a59b89c176b9caa4e73feab0fb2a878635c3324a
David Vogel <david@davidvogel.xyz>
2025-12-02 21:20:52 -0700

    UPDATE: completed "2025, day 2, part 2" in Python

--- 2025-12_AoC-2025/python/day02.py
+++ 2025-12_AoC-2025/python/day02.py
@@ -40,12 +40,55 @@ def part_one(arr):
     print(f"2025 D02 P1 = {sum}")
 
 
+def is_repetitive_part_two(num):
+    length = int(len(num))
+    mid = int(length / 2)
+    i = 1
+
+    while i <= mid:
+        left = num[0:i]
+        sub = num.replace(left, "")
+
+        if sub == "":
+            return True
+
+        i += 1
+
+    return False
+
+
+def part_two(arr):
+    sum = 0
+
+    for line in arr:
+        chunks = line.split(",")
+
+        for chunk in chunks:
+            if chunk == "":
+                continue
+
+            dunks = chunk.split("-")
+            lo = int(dunks[0])
+            hi = int(dunks[1])
+
+            while lo < hi:
+                if is_repetitive_part_two(str(lo)):
+                    sum += lo
+
+                lo += 1
+
+    print(f"2025 D02 P2 = {sum}")
+
+
 def main():
     with open("../inputs/d02.txt", "r") as file:
         arr = file.read().strip("\n").split("\n")
 
     # PART ONE
     part_one(arr)
+
+    # PART TWO
+    part_two(arr)
 
 
 if __name__ == "__main__":
