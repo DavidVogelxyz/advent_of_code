26fb8ec9644525ee99e6579d4ec947c70b1ef90d
David Vogel <david@davidvogel.xyz>
2025-12-02 22:54:13 -0700

    UPDATE: completed "2025, day 3, part 1" in Bash

---
+++ 2025-12_AoC-2025/bash/day03.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+INPUT_FILE="../inputs/d03.txt"
+
+part_one() {
+    local sum=0
+
+    while read -r line; do
+        local count="${#line}"
+        local num_largest=0
+        local num_second=0
+        local pos_largest=0
+        local curr_num=0
+
+        # Get the largest number that's not the last number
+        for ((i=0; i < count - 1; i++)); do
+            curr_num="${line:$i:1}"
+
+            if ((num_largest < curr_num)); then
+                num_largest="$curr_num"
+                pos_largest="$i"
+            fi
+        done
+
+        local next_pos="$((pos_largest + 1))"
+
+        # Get the second largest number to the right of `$num_largest`
+        for ((i="$next_pos"; i < count; i++)); do
+            curr_num="${line:$i:1}"
+
+            if ((num_second < curr_num)); then
+                num_second="$curr_num"
+            fi
+        done
+
+        local jolt="${num_largest}${num_second}"
+        sum="$((sum + jolt))"
+    done < "$INPUT_FILE"
+
+    echo "2025 D03 P1 = $sum"
+}
+
+main() {
+    # PART ONE
+    part_one
+}
+
+main
