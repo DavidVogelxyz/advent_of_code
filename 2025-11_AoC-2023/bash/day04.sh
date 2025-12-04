2197c7f12e64c41325f77c2c5c0e150a2ca249a4
David Vogel <david@davidvogel.xyz>
2025-12-03 21:19:55 -0700

    UPDATE: completed "2023, day 4, part 1" in Bash

---
+++ 2025-11_AoC-2023/bash/day04.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+INPUT_FILE="../inputs/d04.txt"
+
+part_one() {
+    local sum=0
+
+    while IFS="|" read -r card values; do
+        local count=0
+        local points=0
+
+        # Get the card's number
+        local num_card="${card%%:*}"
+        num_card="${num_card##* }"
+
+        # Get the card's numbers into an array
+        local nums_card="${card##*: }"
+        nums_card=($nums_card)
+
+        # Get the winning numbers into an array
+        local my_nums=($values)
+
+        for ((i=0; i<"${#nums_card[@]}"; i++)); do
+            for ((j=0; j<"${#my_nums[@]}"; j++)); do
+                if ((nums_card[$i] == my_nums[$j])); then
+                    count="$((count + 1))"
+                    continue
+                fi
+            done
+        done
+
+        if ((count != 0)); then
+            points="$((2 ** (count - 1)))"
+        fi
+
+        sum="$((sum + points))"
+    done < "$INPUT_FILE"
+
+    echo "2023 D04 P1 = $sum"
+}
+
+main() {
+    # PART ONE
+    part_one
+}
+
+main
