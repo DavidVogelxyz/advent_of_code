44ada1ba231b82b5845d2db0650d70458c11254c
David Vogel <david@davidvogel.xyz>
2025-12-04 22:43:44 -0700

    BREAKING: attempted to solve "2025, day 5, part 2" in Python
    the bad implementation of "2025, day 5, part 2, Bash" was rewritten in Python, enabling the code to fail faster.
    
    running this code will result in trillions of additions to the hashmap, which will cause most computers to run out of memory (OOM) and result in a crash.

--- 2025-12_AoC-2025/python/day05.py
+++ 2025-12_AoC-2025/python/day05.py
@@ -22,12 +22,35 @@ def part_one(arr):
     print(f"2025 D05 P1 = {sum}")
 
 
+def part_two(arr):
+    valids = []
+    sum = 0
+
+    for line in arr:
+        if line.__contains__("-"):
+            nums = line.split("-")
+            left = int(nums[0])
+            right = int(nums[1])
+
+            for num in range(left, right + 1):
+                valids += [num]
+        else:
+            continue
+
+    sum = len(valids)
+
+    print(f"2025 D05 P2 = {sum}")
+
+
 def main():
     with open("../inputs/d05.txt", "r") as file:
         arr = file.read().strip("\n").split("\n")
 
     # PART ONE
     part_one(arr)
+
+    # PART TWO
+    part_two(arr)
 
 
 if __name__ == "__main__":
