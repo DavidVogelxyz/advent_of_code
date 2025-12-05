168f18051854840c3bff2d163f6fe1d26e436998
David Vogel <david@davidvogel.xyz>
2025-12-05 00:57:24 -0700

    UPDATE: completed "2025, day 5, part 2" in Bash

--- 2025-12_AoC-2025/bash/day05.sh
+++ 2025-12_AoC-2025/bash/day05.sh
@@ -33,9 +33,6 @@ part_one() {
                 fi
             done
 
-            # Once the checks complete, push the range to `valids`
-            valids+=("${left}-${right}")
-
             # If any IDs in `remove`, unset them
             for id in "${remove[@]}"; do
                 unset valids[$id]
@@ -46,6 +43,9 @@ part_one() {
 
             # Empty the `remove` array
             remove=()
+
+            # Once the checks complete, push the range to `valids`
+            valids+=("${left}-${right}")
         elif [[ "$line" == "" ]]; then
             continue
         else
@@ -66,9 +66,80 @@ part_one() {
     echo "2025 D05 P1 = $sum"
 }
 
+part_two() {
+    local valids=()
+    local sum=0
+
+    # Get all ranges into an array (`valids`)
+    while read -r line; do
+        if [[ "$line" =~ "-" ]]; then
+            valids+=("$line")
+        else
+            continue
+        fi
+    done < "$INPUT_FILE"
+
+    # Attempt to merge ranges
+    # If successful merge, add to `valids`; then, remove original ranges
+    for range in "${valids[@]}"; do
+        local left="${range%%-*}"
+        local right="${range##*-}"
+        local remove=()
+
+        # Loop through all current ranges in `valid`; see if any can be merged
+        for ((i=0; i < "${#valids[@]}"; i++)); do
+            local low="${valids[$i]%%-*}"
+            local high="${valids[$i]##*-}"
+
+            # Check if `$left` is between `valids`
+            if ((low <= left)) && ((left <= high)); then
+                left="$low"
+            fi
+
+            # Check if `$right` is between `valids`
+            if ((low <= right)) && ((right <= high)); then
+                right="$high"
+            fi
+
+            # If either changes, add the `$i` so it can be removed
+            if ((left == low)) || ((right == high)); then
+                remove+=("$i")
+            fi
+        done
+
+        # If any IDs in `remove`, unset them
+        for id in "${remove[@]}"; do
+            unset valids[$id]
+        done
+
+        # Reorder the `valids` array
+        valids=(${valids[@]})
+
+        # Empty the `remove` array
+        remove=()
+
+        # Once the checks complete, push the range to `valids`
+        valids+=("${left}-${right}")
+    done
+
+    # Loop through merged ranges to sum the diffs (inclusive)
+    for range in "${valids[@]}"; do
+        local low="${range%%-*}"
+        local high="${range##*-}"
+        local diff_inclu="$((high - low + 1))"
+
+        sum="$((sum + diff_inclu))"
+    done
+
+    echo "2025 D05 P2 = $sum"
+}
+
 main() {
     # PART ONE
     part_one
+
+    # PART TWO
+    part_two
 }
 
 main
