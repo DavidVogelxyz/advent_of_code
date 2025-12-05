cdaf1f6d60e9316819de6f22fdbd56463f672a6f
David Vogel <david@davidvogel.xyz>
2025-12-04 23:34:14 -0700

    UPDATE: "2025, day 5, part 1, Bash" now merges valid ranges

--- 2025-12_AoC-2025/bash/day05.sh
+++ 2025-12_AoC-2025/bash/day05.sh
@@ -8,10 +8,48 @@ part_one() {
 
     while read -r line; do
         if [[ "$line" =~ "-" ]]; then
-            valids+=("$line")
+            local left="${line%%-*}"
+            local right="${line##*-}"
+            local remove=()
+
+            for ((i=0; i < "${#valids[@]}"; i++)); do
+                range="${valids[$i]}"
+                local low="${range%%-*}"
+                local high="${range##*-}"
+
+                # Check if `$left` is between `valids`
+                if ((low <= left)) && ((left <= high)); then
+                    left="$low"
+                fi
+
+                # Check if `$right` is between `valids`
+                if ((low <= right)) && ((right <= high)); then
+                    right="$high"
+                fi
+
+                # If either changes, add the `$i` so it can be removed
+                if ((left == low)) || ((right == high)); then
+                    remove+=("$i")
+                fi
+            done
+
+            # Once the checks complete, push the range to `valids`
+            valids+=("${left}-${right}")
+
+            # If any IDs in `remove`, unset them
+            for id in "${remove[@]}"; do
+                unset valids[$id]
+            done
+
+            # Reorder the `valids` array
+            valids=(${valids[@]})
+
+            # Empty the `remove` array
+            remove=()
         elif [[ "$line" == "" ]]; then
             continue
         else
+            # Check if the number is within any valid ranges
             for ((i=0; i < "${#valids[@]}"; i++)); do
                 valid="${valids[$i]}"
                 left="${valid%%-*}"
@@ -28,35 +66,9 @@ part_one() {
     echo "2025 D05 P1 = $sum"
 }
 
-part_two() {
-    declare -A valids
-    local sum=0
-
-    while read -r line; do
-        if [[ "$line" =~ "-" ]]; then
-            left="${line%%-*}"
-            right="${line##*-}"
-
-            for ((i="$left"; i <= "$right"; i++)); do
-                echo "looked at $i"
-                valids[$i]=1
-            done
-        else
-            continue
-        fi
-    done < "$INPUT_FILE"
-
-    sum="${#valids[@]}"
-
-    echo "2025 D05 P2 = $sum"
-}
-
 main() {
     # PART ONE
     part_one
-
-    # PART TWO
-    part_two
 }
 
 main
